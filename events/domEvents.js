e4c83cacc18e400ff572d11ac6b8484699c54e1f
Jonah Thomas <jonahthomas20@yahoo.com>
2025-03-15 08:14:42 -0500

    added read filter delete for books.

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,3 +1,4 @@
+import { deleteBook, getBooks, getSingleBook } from '../helpers/data/bookData';
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     // TODO: CLICK EVENT FOR DELETING A BOOK
@@ -15,14 +16,16 @@ const domEvents = () => {
 
     // TODO: CLICK EVENT FOR SHOWING FORM FOR ADDING A BOOK
     if (e.target.id.includes('add-book-btn')) {
-      console.warn('ADD BOOK');
+      addBookForm();
     }
 
     // TODO: CLICK EVENT EDITING/UPDATING A BOOK
     if (e.target.id.includes('edit-book-btn')) {
-      console.warn('EDIT BOOK', e.target.id);
-      console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+    
+      getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
     }
+
     // TODO: CLICK EVENT FOR VIEW BOOK DETAILS
     if (e.target.id.includes('view-book-btn')) {
       console.warn('VIEW BOOK', e.target.id);
